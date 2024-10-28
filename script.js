a279fe1319ae8f132b24f3e27179fce1e9de412d
Justin M <manningjustin08@yahoo.com>
2024-10-28 09:36:52 -0400

    added images and dog descriptions

--- script.js
+++ script.js
@@ -2,19 +2,32 @@ const BASE_URL = "https://dog.ceo/api/";
 const fido = document.getElementById("fido");
 const lucy = document.getElementById("lucy");
 const rook = document.getElementById("rook");
+const fido_img = document.getElementById('img1')
+const lucy_img = document.getElementById('img2')
+const rook_img = document.getElementById('img3')
 
-fido.addEventListener("click", async () => {
-  const response = await axios.get(`${BASE_URL}breed/affenpinscher/images`);
+async function loadImage1() {
+     const response = await axios.get(`${BASE_URL}breed/affenpinscher/images`);
   const img =  response.data.message[0]
+    fido_img.setAttribute('src', img)
+}
+ 
+loadImage1()
 
-});
 
-lucy.addEventListener("click", async () => {
-  const response = await axios.get(`${BASE_URL}breed/akita/images`);
+async function loadImage2() {
+      const response = await axios.get(`${BASE_URL}breed/akita/images`);
   const img =  response.data.message[0]
-});
+  lucy_img.setAttribute('src', img)
+}
 
-rook.addEventListener("click", async () => {
-  const response = await axios.get(`${BASE_URL}breed/whippet/images`);
+loadImage2()
+
+async function loadImage3() {
+const response = await axios.get(`${BASE_URL}breed/whippet/images`);
   const img =  response.data.message[0]
-});
+  rook_img.setAttribute('src', img)
+}
+
+loadImage3()
+
