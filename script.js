02257493a01c4ab4edafad0032e9f4937a7bd054
Justin M <manningjustin08@yahoo.com>
2024-10-28 14:28:59 -0400

    added shelter landing pics and padding

--- script.js
+++ script.js
@@ -2,76 +2,111 @@ const BASE_URL = "https://dog.ceo/api/";
 const fido = document.getElementById("fido");
 const lucy = document.getElementById("lucy");
 const rook = document.getElementById("rook");
-const img1 = document.getElementById('img1')
-const img2 = document.getElementById('img2')
-const img3 = document.getElementById('img3')
-const header1 = document.getElementById('firName')
-const header2 = document.getElementById('secName')
-const header3 = document.getElementById('thirName')
-const dropdown = document.getElementById('categories')
-const next = document.getElementById('next')
-const names = ["Bella", "Max", "Luna", "Charlie", "Lucy", "Cooper", "Daisy", "Milo", "Sadie", "Rocky",
-    "Molly", "Buddy", "Bailey", "Stella", "Bear", "Lola", "Duke", "Roxy", "Teddy", "Zoe",
-    "Lily", "Bentley", "Coco", "Winston", "Nala", "Riley", "Ruby", "Finn", "Gracie", "Zeus",
-    "Willow", "Murphy", "Piper", "Jake", "Loki", "Rosie", "Diesel", "Ginger", "Harley", "Scout",
-    "Lucky", "Millie", "Koda", "Abby", "Remy", "Sammy", "Blue", "Thor", "Moose", "Ranger"]
-
-
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const header1 = document.getElementById("firName");
+const header2 = document.getElementById("secName");
+const header3 = document.getElementById("thirName");
+const dropdown = document.getElementById("categories");
+const next = document.getElementById("next");
+const names = [
+  "Bella",
+  "Max",
+  "Luna",
+  "Charlie",
+  "Lucy",
+  "Cooper",
+  "Daisy",
+  "Milo",
+  "Sadie",
+  "Rocky",
+  "Molly",
+  "Buddy",
+  "Bailey",
+  "Stella",
+  "Bear",
+  "Lola",
+  "Duke",
+  "Roxy",
+  "Teddy",
+  "Zoe",
+  "Lily",
+  "Bentley",
+  "Coco",
+  "Winston",
+  "Nala",
+  "Riley",
+  "Ruby",
+  "Finn",
+  "Gracie",
+  "Zeus",
+  "Willow",
+  "Murphy",
+  "Piper",
+  "Jake",
+  "Loki",
+  "Rosie",
+  "Diesel",
+  "Ginger",
+  "Harley",
+  "Scout",
+  "Lucky",
+  "Millie",
+  "Koda",
+  "Abby",
+  "Remy",
+  "Sammy",
+  "Blue",
+  "Thor",
+  "Moose",
+  "Ranger",
+];
 
 async function loadImage() {
-     const response = await axios.get(`${BASE_URL}breeds/list/all`);
-     const breedArr = response.data.message
+  const response = await axios.get(`${BASE_URL}breeds/list/all`);
+  const breedArr = response.data.message;
 
-     for(let breed in breedArr){
-        let newOption = document.createElement('option')
-        newOption.textContent = breed
-        dropdown.appendChild(newOption)
-     }
+  for (let breed in breedArr) {
+    let newOption = document.createElement("option");
+    newOption.textContent = breed;
+    dropdown.appendChild(newOption);
+  }
 
-     dropdown.addEventListener('change', async (event) => {
-       const breedID = event.target.value
-       const response = await axios.get(`${BASE_URL}breed/${breedID}/images`);
-       const imgList = response.data.message
-        
-       header1.textContent = 'Adopt'
-       header2.textContent = 'Our'
-       header3.textContent = `${breedID}s`
+  img1.style.backgroundImage = "URL(/images/holdingdog1.jpg)";
+  img2.style.backgroundImage = "URL(/images/holdingdog2.jpg)";
+  img3.style.backgroundImage = "URL(/images/holdingdog3.jpg)";
 
 
 
-       let counter = 0 
-        img1.setAttribute('src', imgList[counter])
-        img2.setAttribute('src', imgList[counter+=1])
-        img3.setAttribute('src', imgList[counter+=1])
+  dropdown.addEventListener("change", async (event) => {
+    const breedID = event.target.value;
+    const response = await axios.get(`${BASE_URL}breed/${breedID}/images`);
+    const imgList = response.data.message;
 
-        next.addEventListener('click', ()=> {
+    header1.textContent = "Adopt";
+    header2.textContent = "Our";
+    header3.textContent = `${breedID}s`;
 
-         let num1 = Math.random() * 50
-         let num2 = Math.random() * 50
-         let num3 = Math.random() * 50
+    let counter = 0;
+    img1.setAttribute("src", imgList[counter]);
+    img2.setAttribute("src", imgList[(counter += 1)]);
+    img3.setAttribute("src", imgList[(counter += 1)]);
 
+    next.addEventListener("click", () => {
+      let num1 = Math.random() * 50;
+      let num2 = Math.random() * 50;
+      let num3 = Math.random() * 50;
 
-         header1.textContent = names[Math.floor(num1)]
-         header2.textContent = names[Math.floor(num2)]
-         header3.textContent = names[Math.floor(num3)]
+      header1.textContent = names[Math.floor(num1)];
+      header2.textContent = names[Math.floor(num2)];
+      header3.textContent = names[Math.floor(num3)];
 
+      img1.setAttribute("src", imgList[counter++]);
+      img2.setAttribute("src", imgList[counter++]);
+      img3.setAttribute("src", imgList[counter++]);
+    });
+  });
+}
 
-
-           
-
-            img1.setAttribute('src', imgList[counter++])
-            img2.setAttribute('src', imgList[counter++])
-            img3.setAttribute('src', imgList[counter++])
-        })
-       
-
-
-
-     })
-
-
-}     
-
-loadImage()
- 
-
+loadImage();
