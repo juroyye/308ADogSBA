6f7bf496b85e816201495080046dcec7a2578ec3
Justin M <manningjustin08@yahoo.com>
2024-10-28 13:14:28 -0400

    refactored with counter and api changes

--- script.js
+++ script.js
@@ -2,32 +2,52 @@ const BASE_URL = "https://dog.ceo/api/";
 const fido = document.getElementById("fido");
 const lucy = document.getElementById("lucy");
 const rook = document.getElementById("rook");
-const fido_img = document.getElementById('img1')
-const lucy_img = document.getElementById('img2')
-const rook_img = document.getElementById('img3')
+const img1 = document.getElementById('img1')
+const img2 = document.getElementById('img2')
+const img3 = document.getElementById('img3')
+const dropdown = document.getElementById('categories')
+const next = document.getElementById('next')
 
-async function loadImage1() {
-     const response = await axios.get(`${BASE_URL}breed/affenpinscher/images`);
-  const img =  response.data.message[0]
-    fido_img.setAttribute('src', img)
-}
+
+async function loadImage() {
+     const response = await axios.get(`${BASE_URL}breeds/list/all`);
+     const breedArr = response.data.message
+
+     for(let breed in breedArr){
+        let newOption = document.createElement('option')
+        newOption.textContent = breed
+        dropdown.appendChild(newOption)
+     }
+
+     dropdown.addEventListener('change', async (event) => {
+       const breedID = event.target.value
+       const response = await axios.get(`${BASE_URL}breed/${breedID}/images`);
+       const imgList = response.data.message
+
+       let counter = 0 
+        img1.setAttribute('src', imgList[counter])
+        img2.setAttribute('src', imgList[counter+=1])
+        img3.setAttribute('src', imgList[counter+=1])
+
+        next.addEventListener('click', ()=> {
+
+            if(counter == 10){
+                next.style.display = 'none';
+            }
+
+            img1.setAttribute('src', imgList[counter++])
+            img2.setAttribute('src', imgList[counter++])
+            img3.setAttribute('src', imgList[counter++])
+        })
+       
+
+
+
+     })
+
+
+}     
+
+loadImage()
  
-loadImage1()
-
-
-async function loadImage2() {
-      const response = await axios.get(`${BASE_URL}breed/akita/images`);
-  const img =  response.data.message[0]
-  lucy_img.setAttribute('src', img)
-}
-
-loadImage2()
-
-async function loadImage3() {
-const response = await axios.get(`${BASE_URL}breed/whippet/images`);
-  const img =  response.data.message[0]
-  rook_img.setAttribute('src', img)
-}
-
-loadImage3()
 
